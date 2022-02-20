a8199e9942db29cdc3c7b6ee1f729bf31ef4418c
ryan <rsheehan@gmail.com>
2022-02-19 20:35:16 -0600

    broken postcess

---
+++ src/routes/__layout.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">  
+</script>
+
+<slot/>
+
+<style lang="postcss">
+  :global {
+    @import '../styles/global.css';
+    @import 'open-props/postcss/style';
+    @import 'open-props/postcss/normalize';    
+  }
+</style>
--- svelte.config.js
+++ svelte.config.js
@@ -1,11 +1,33 @@
 import adapter from '@sveltejs/adapter-auto';
 import preprocess from 'svelte-preprocess';
+import autoprefixer from 'autoprefixer';
+import presetEnv from 'postcss-preset-env';
+import postcssJitProps from 'postcss-jit-props';
+import OpenProps from 'open-props';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	// Consult https://github.com/sveltejs/svelte-preprocess
 	// for more information about preprocessors
-	preprocess: preprocess(),
+	preprocess: preprocess({    
+    postcss: {
+      plugins: [
+        postcssJitProps(OpenProps),
+        autoprefixer(),
+        presetEnv({
+          features: {
+            'nesting-rules': true,
+            'custom-properties': true,
+            'gap-properties': true,
+            'not-pseudo-class': true,
+            'custom-media-queries': {
+              importFrom: 'src/styles/queries.pcss',
+            },
+          },
+        }),
+      ]
+    },
+  }),
 
 	kit: {
 		adapter: adapter()
